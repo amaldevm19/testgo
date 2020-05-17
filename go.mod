b941f5949473d9fec8f08d2abe5be495ad557128
amaldevm19 <amaldevm19@gmail.com>
2020-05-17 10:27:02 +0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amaldevm19/testgo
+
+go 1.14
---
+++ testgo.go
@@ -0,0 +1,9 @@
+package testgo
+
+import "fmt"
+
+//GoTest for testing
+func GoTest() {
+
+	fmt.Println("This is printed by testgo module")
+}
